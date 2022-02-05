58a836861ef3c608a3909ef4a7b96ca6eb36aadd
ouakala <mr.ouakala.abdelaaziz@gmail.com>
2022-02-05 16:48:26 +0100

    Task 21: refactor upload video angular component

--- frontend/youtubecloneclient/src/app/services/video-upload.service.ts
+++ frontend/youtubecloneclient/src/app/services/video-upload.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { UploadVideoResponse } from '../models/upload-video-response';
 
 @Injectable({
   providedIn: 'root',
@@ -10,10 +11,10 @@ export class VideoUploadService {
   private baseUrl: string = environment.baseUrl;
   constructor(private _httpClient: HttpClient) {}
 
-  public uploadVideo(fileEntry: File): Observable<any> {
+  public uploadVideo(fileEntry: File): Observable<UploadVideoResponse> {
     const formData = new FormData();
     formData.append('file', fileEntry, fileEntry.name);
-    return this._httpClient.post(
+    return this._httpClient.post<UploadVideoResponse>(
       `${this.baseUrl}/videos/upload-video`,
       formData
     );
