20aed5c75574c787cc89c36109b6fb0d1c5ded6b
ouakala <mr.ouakala.abdelaaziz@gmail.com>
2022-02-05 18:18:26 +0100

    Task 25: update save video details component (add more material components)

--- frontend/youtubecloneclient/src/app/app.module.ts
+++ frontend/youtubecloneclient/src/app/app.module.ts
@@ -5,28 +5,44 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { UploadVideoComponent } from './components/upload-video/upload-video.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { NgxFileDropModule } from 'ngx-file-drop';
 import { MatButtonModule } from '@angular/material/button';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatIconModule} from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 import { HeaderComponent } from './components/header/header.component';
 import { SaveVideoDetailsComponent } from './components/save-video-details/save-video-details.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatOptionModule } from '@angular/material/core';
+import { MatChipsModule } from '@angular/material/chips';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
-  declarations: [AppComponent, UploadVideoComponent, HeaderComponent, SaveVideoDetailsComponent],
+  declarations: [
+    AppComponent,
+    UploadVideoComponent,
+    HeaderComponent,
+    SaveVideoDetailsComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpClientModule,
     NgxFileDropModule,
     MatButtonModule,
     MatToolbarModule,
     MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatOptionModule,
+    MatChipsModule,
     FlexLayoutModule,
   ],
   providers: [],
--- frontend/youtubecloneclient/src/app/components/save-video-details/save-video-details.component.ts
+++ frontend/youtubecloneclient/src/app/components/save-video-details/save-video-details.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-save-video-details',
   templateUrl: './save-video-details.component.html',
-  styleUrls: ['./save-video-details.component.css']
+  styleUrls: ['./save-video-details.component.css'],
 })
 export class SaveVideoDetailsComponent implements OnInit {
+  public videoDetailsFrom: FormGroup = new FormGroup({
+    title: new FormControl(''),
+    description: new FormControl(''),
+    videoStatus: new FormControl(''),
+  });
 
-  constructor() { }
+  constructor(private _formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
+    this.videoDetailsFrom = this._formBuilder.group({
+      title: [''],
+      description: [''],
+      videoStatus: [''],
+    });
   }
-
 }
